bac0a6e1e18d6d6fc2d96e7f86a782ad6cdc1f02
ez4real <butilka05roma@gmail.com>
2024-10-22 21:12:28 -0400

    add sidebar menu

--- frontend/src/Components/Header/index.jsx
+++ frontend/src/Components/Header/index.jsx
@@ -1,16 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './index.css'
 
 const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
-    <header>
-      <div className='header-container'>
-        <div><img src='/logo.svg' alt='Anumo Logo'></img></div>
-        <div className='textCenter'><img src='/menu-burger.svg' alt='Burger Menu'></img>
+    <>
+      <header>
+        <div className='header-container'>
+          <div><img src='/logo.svg' alt='Anumo Logo'></img></div>
+          <div className='textCenter'>
+            <button className='burgerMenuButton' onClick={toggleMenu}>
+              <img src='/menu-burger.svg' alt='Burger Menu'></img>
+            </button>
+          </div>
+          <div className='textEnd'><img src='/shopping-bag.svg' alt='Anumo Logo'></img></div>
         </div>
-        <div className='textEnd'><img src='/logo.svg' alt='Anumo Logo'></img></div>
+      </header>
+
+      <div className={`sidebarMenu ${isMenuOpen ? 'open' : ''}`}>
+        <div className={'sidebarMenu-container'}>
+          <div className='textEnd'><img src='/shopping-bag-black.svg' alt='Anumo Logo'></img></div>
+          <ul className='menuLinks'>           
+            <li><a href='#'>HOME</a></li>
+            <li><a href='#'>SHOP</a></li>
+            <li><a href='#'>PROJECTS</a></li>
+            <li><a href='#'>ABOUT</a></li>
+            <li><a href='#'>CONTACT US</a></li>
+            <li><a href='#'>FIND US</a></li>
+          </ul>
+          <div className='menuFooter'>
+            <div className='menuFooter-container'>
+              <ul>
+                <li>Privacy Policy</li>
+                <li>Payment and Delivery</li>
+                <li>Returns</li>
+              </ul>
+              <div className='languageCurrency-container'>
+                <div className='language'>ENG</div>
+                <div className='currency'>USD</div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-    </header>
+
+      {isMenuOpen && <div className='overlay' onClick={toggleMenu}></div>}
+    </>
   );
 };
 
