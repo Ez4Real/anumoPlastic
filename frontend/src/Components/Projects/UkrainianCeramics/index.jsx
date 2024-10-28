8e388118266c34d4b54678ebce574adac0cdb934
ez4real <butilka05roma@gmail.com>
2024-10-28 15:30:23 -0400

    add ukrainian ceramics component

---
+++ frontend/src/Components/Projects/UkrainianCeramics/index.jsx
@@ -0,0 +1,53 @@
+
+
+import "./index.css"
+import { useTranslation } from 'react-i18next';
+import { importAll } from "../../../utils/importRecources";
+
+
+const images = importAll(require.context('../../../assets/images/projects/ukrainianCeramics/', false, /\.(png|jpe?g|svg)$/));
+
+
+const UkrainianCeramics = () => {
+  const { t } = useTranslation();
+
+
+  return (
+    <div className='projectComponent-container'>
+        <p className='projectTitle'>{t('ProjectsPage.projects.ukrainianCeramics.title')}<span >/2022</span></p>
+        <div className="ukrainianCeramics-container">
+            <div className="leftColumn">
+                <div className="leftImage-container">
+                    <img src={images["gorschykX.png"]}></img>
+                </div>
+                <div className="leftImage-container">
+                    <img src={images["gorschykH.png"]}></img>
+                </div>
+                <div className="leftImage-container">
+                    <img src={images["gorschykH_1.png"]}></img>
+                </div>
+            </div>
+            <div className="rightColumn">
+                <div className="grid2x2Row">
+                    <div><img src={images["gorschykX_1.png"]}></img></div>
+                    <div><img src={images["gorschykX_2.png"]}></img></div>
+                </div>
+                <div className="grid2x2Row">
+                    <div><img src={images["glechyk_1.png"]}></img></div>
+                    <div><img src={images["glechyk_2.png"]}></img></div>
+                </div>
+                <div>
+                    <div><img src={images["glechyk.png"]}></img></div>
+                </div>
+                <div className="description-container">
+                    <p className="projectDescription">{t('ProjectsPage.projects.ukrainianCeramics.description')}</p>
+                </div>
+            </div>
+            
+        </div>
+    </div>
+
+  );
+};
+
+export default UkrainianCeramics;
--- frontend/src/Pages/Projects/index.jsx
+++ frontend/src/Pages/Projects/index.jsx
@@ -3,22 +3,26 @@ import "./index.css"
 import { useTranslation } from 'react-i18next';
 import SunnyBunny from '../../Components/Projects/SunnyBunny';
 import Cooperation from '../../Components/Cooperation';
+import UkrainianCeramics from '../../Components/Projects/UkrainianCeramics';
 
 const Projects = () => {
   const [currentProjectIndex, setCurrentProjectIndex] = useState(0);
 
   const { t } = useTranslation();
-//   const goToNextProject = () => {
-//     setCurrentProjectIndex((prevIndex) =>
-//       prevIndex === projects.length - 1 ? 0 : prevIndex + 1
-//     );
-//   };
+  
+  const goToNextProject = () => {
+    setCurrentProjectIndex((prevIndex) =>
+      prevIndex === projects.length - 1 ? 0 : prevIndex + 1
+    );
+  };
 
-//   const goToPreviousProject = () => {
-//     setCurrentProjectIndex((prevIndex) =>
-//       prevIndex === 0 ? projects.length - 1 : prevIndex - 1
-//     );
-//   };
+  const goToPreviousProject = () => {
+    setCurrentProjectIndex((prevIndex) =>
+      prevIndex === 0 ? projects.length - 1 : prevIndex - 1
+    );
+  };
+
+  const projects = [<SunnyBunny />, <UkrainianCeramics/>,];
 
   return (
     <div className='content'>
@@ -27,13 +31,17 @@ const Projects = () => {
         <span className='pageName'>{t('ProjectsPage.pageName')}</span>
       </p>
 
-      <div className="project-slider">
-        {/* <SunnyBunny/> */}
+      <div className="projectsSlider">
+        {projects[currentProjectIndex]}
       </div>
 
       <div className='sliderFooter-container'>
-        <div><img src='/arrow-left.svg'></img><span>{t('ProjectsPage.back')}</span></div>
-        <div><span>{t('ProjectsPage.next')}</span><img src='/arrow-right.svg'></img></div>
+        <div onClick={goToPreviousProject}>
+          <img src='/arrow-left.svg'></img><span>{t('ProjectsPage.back')}</span>
+        </div>
+        <div onClick={goToNextProject}>
+          <span>{t('ProjectsPage.next')}</span><img src='/arrow-right.svg'></img>
+        </div>
       </div>
 
       <Cooperation />
